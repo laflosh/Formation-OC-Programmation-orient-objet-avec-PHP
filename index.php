5ca9104fc85046b2702eb98ca2de88e2acc1e5c6
laflosh <l_florent_l@hotmail.fr>
2024-04-08 15:54:33 +0200

    Méthodes communes aux objets

--- index.php
+++ index.php
@@ -25,31 +25,46 @@ class Encounter {
 
 class Player {
 
-    public int $level;
+    private int $level;
 
-    public function getLevelPlayer() : int {
+    public function getLevel() : int {
         return $this->level;
     }
 
-    public function setLevelPlayer(int $level) : void {
-
-        if($level < 0 ){
-            trigger_error("Score non valide");
-        }
+    public function setLevel(int $level): void
+    {
+        
+        self::checkLevel($level);
 
         $this->level = $level;
 
+    }
+
+    public function __construct(int $level){
+
+        self::checkLevel($level);
+
+        $this->level = $level;
+
+    }
+
+    public static function checkLevel($level){
+
+        if($level < 0 ){
+
+            trigger_error("Score non valide");
+
+        }
+
+        return true;
     }
 
 }
 
 
 //Initiation du score
-$greg = new Player();
-$jade = new Player();
-
-$greg->setLevelPlayer(400);
-$jade->setLevelPlayer(800);
+$greg = new Player(400);
+$jade = new Player(800);
 
 echo sprintf(
     'Greg à %.2f%% chance de gagner face a Jade',
@@ -62,8 +77,8 @@ Encounter::setNewLevel($greg, $jade, Encounter::RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
-    $greg->level,
-    $jade->level
+    $greg->getLevel(),
+    $jade->getLevel()
 );
 
 exit(0);
