65f21da3aec17f04b9f0ceef7dd7e58ec898c6f8
laflosh <l_florent_l@hotmail.fr>
2024-04-08 15:05:09 +0200

    premier setter et getter

--- index.php
+++ index.php
@@ -7,8 +7,36 @@ const RESULT_POSSIBILITIES = [RESULT_WINNER, RESULT_LOSER, RESULT_DRAW];
 
 class Encounter {
 
-    public int $player1;
-    public int $player2;
+    private int $player1;
+    private int $player2;
+
+    public function getScorePlayer1() : int {
+        return $this->player1;
+    }
+
+    public function getScorePlayer2() : int {
+        return $this->player2;
+    }
+
+    public function setScorePlayer1(int $score) : void {
+
+        if($score < 0 ){
+            trigger_error("Score non valide");
+        }
+
+        $this->player1 = $score;
+
+    }
+
+    public function setScorePlayer2(int $score) : void {
+        
+        if($score < 0 ){
+            trigger_error("Score non valide");
+        }
+
+        $this->player2 = $score;
+
+    }
 
     public function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
     {
@@ -27,23 +55,26 @@ class Encounter {
 }
 
 $match = new Encounter();
+//Initiation du score
+$match->setScorePlayer1(400);
+$match->setScorePlayer2(800);
 
-$match->player1 = 400;
-$match->player2 = 800;
+$greg = $match->getScorePlayer1();
+$jade = $match->getScorePlayer2();
 
 echo sprintf(
     'Greg à %.2f%% chance de gagner face a Jade',
-    $match->probabilityAgainst($match->player1, $match->player2)*100
+    $match->probabilityAgainst($greg, $jade)*100
 ).PHP_EOL;
 
 // Imaginons que greg l'emporte tout de même.
-$match->setNewLevel($match->player1, $match->player2, RESULT_WINNER);
-$match->setNewLevel($match->player1, $match->player2, RESULT_LOSER);
+$match->setNewLevel($greg, $jade, RESULT_WINNER);
+$match->setNewLevel($greg, $jade, RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
-    $match->player1,
-    $match->player2
+    $greg,
+    $jade
 );
 
 exit(0);
